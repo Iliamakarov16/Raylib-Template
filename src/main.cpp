51a1f591c2118d321bb22b7c3c411419ee479b19
Illia <147354919+Iliamakarov16@users.noreply.github.com>
2026-01-07 20:18:48 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,18 @@
 #include <raylib.h>
-#include "ball.h"
 
 int main() 
 {
-    const Color darkGreen = {20, 160, 133, 255};
     
     constexpr int screenWidth = 800;
     constexpr int screenHeight = 600;
     
-    Ball ball;
-    
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    InitWindow(screenWidth, screenHeight, "Game name");
     SetTargetFPS(60);
     
     while (!WindowShouldClose())
     {
-        ball.Update();
-        
         BeginDrawing();
-            ClearBackground(darkGreen);
-            ball.Draw();
+        ClearBackground(BLACK);
         EndDrawing();
     }
     
